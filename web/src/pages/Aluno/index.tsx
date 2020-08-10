cc4eb1cd7e6ae8125e0864c758a55b8cf2af9a64
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2020-08-10 00:49:59 -0300

    add profs

--- web/src/pages/Aluno/index.tsx
+++ web/src/pages/Aluno/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import PageHeader from '../../components/PageHeader';
+import whatsIcone from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
 function Aluno() {
@@ -24,6 +25,33 @@ function Aluno() {
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+               <article className="prof-item">
+                   <header>
+                       <img src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4" alt="Jeef"/>
+                       <div>
+                           <strong>Diego</strong>
+                           <span>Química</span>
+                       </div>
+                   </header>
+                   <p>
+                        Entusiasta das melhores tecnologias de química avançada.
+                        <br /><br />
+                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+                    </p>
+                   <footer>
+                       <p>
+                           Preço/hora
+                           <strong>R$80,00</strong>
+                        </p>
+                        <button type="button" >
+                            <img src={whatsIcone} alt="whats"/>
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     )
 }
